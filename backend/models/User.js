a05a31d73e5cd238dd89869454381db6d6c19c6d
mail2anjaligupta2005-ctrl <you@example.com>
2026-07-05 16:19:52 +0530

    Initial setup with authentication register API

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    branch: {
+      type: String,
+      default: "",
+    },
+
+    semester: {
+      type: Number,
+      default: 1,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
